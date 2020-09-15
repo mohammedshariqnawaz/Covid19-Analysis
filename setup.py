5125eeec968fc5bfe8c768c2d3ea7679ed816015
Mohammed Shariq Nawaz <mohammedshariqn@gmail.com>
2020-09-15 23:46:29 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applied Data Science on Covid19 Data',
+    author='Mohammed Shariq Nawaz',
+    license='',
+)
